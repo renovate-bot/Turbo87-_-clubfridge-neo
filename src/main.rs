c3ce0bf1a894cb20c8380546c4af11a18fb87012
Tobias Bieniek <tobias@bieniek.cloud>
2025-01-29 09:34:32 +0100

    Move `theme()` and `view()` fns into `State` struct

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,13 @@ mod state;
 mod ui;
 
 use crate::state::State;
-use crate::ui::{theme, view};
 use iced::application;
 
 pub fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
 
-    application("ClubFridge neo", State::update, view)
-        .theme(theme)
+    application("ClubFridge neo", State::update, State::view)
+        .theme(State::theme)
         .subscription(State::subscription)
         .resizable(true)
         .window_size((800., 480.))
--- src/ui.rs
+++ src/ui.rs
@@ -4,93 +4,94 @@ use iced::{color, Center, Element, Fill, Right, Theme};
 use rust_decimal::Decimal;
 use std::sync::Arc;
 
-pub fn theme(_state: &State) -> Theme {
-    Theme::Custom(Arc::new(iced::theme::Custom::new(
-        "clubfridge".to_string(),
-        iced::theme::Palette {
-            background: color!(0x000000),
-            text: color!(0xffffff),
-            primary: color!(0xffffff),
-            success: color!(0x4BD130),
-            danger: color!(0xD5A30F),
-        },
-    )))
-}
-
-pub fn view(state: &State) -> Element<Message> {
-    let title = state
-        .user
-        .as_ref()
-        .and_then(|user_id| {
-            state
-                .users
-                .get(user_id)
-                .map(|user_name| text(format!("{user_name} ({user_id})")))
-        })
-        .unwrap_or(text("Bitte RFID Chip"));
-
-    let sum = state.items.iter().map(|item| item.total()).sum::<Decimal>();
-
-    let content = column![
-        title.size(36),
-        scrollable(items(&state.items))
-            .height(Fill)
-            .width(Fill)
-            .anchor_bottom(),
-        text(format!("Summe: {sum:.2}€"))
-            .size(24)
-            .align_x(Right)
-            .width(Fill),
-        row![
-            button(
-                text("Abbruch")
-                    .color(color!(0xffffff))
-                    .size(36)
-                    .align_x(Center)
-            )
-            .width(Fill)
-            .style(button::danger)
-            .padding([10, 20])
-            .on_press_maybe(state.user.as_ref().map(|_| Message::Cancel)),
-            button(
-                text("Bezahlen")
-                    .color(color!(0xffffff))
-                    .size(36)
-                    .align_x(Center)
-            )
-            .width(Fill)
-            .style(button::success)
-            .padding([10, 20])
-            .on_press_maybe(state.user.as_ref().map(|_| Message::Pay)),
-        ]
-        .spacing(10),
-    ]
-    .spacing(10);
-
-    let mut stack = stack![content];
-
-    if state.show_sale_confirmation {
-        stack = stack.push(
-            container(
-                container(
-                    text("Danke für deinen Kauf")
-                        .size(36)
-                        .color(color!(0x000000)),
-                )
-                .style(|_theme: &Theme| container::background(color!(0xffffff)))
-                .padding([15, 30]),
-            )
-            .width(Fill)
-            .height(Fill)
-            .align_x(Center)
-            .align_y(Center),
-        );
+impl State {
+    pub fn theme(&self) -> Theme {
+        Theme::Custom(Arc::new(iced::theme::Custom::new(
+            "clubfridge".to_string(),
+            iced::theme::Palette {
+                background: color!(0x000000),
+                text: color!(0xffffff),
+                primary: color!(0xffffff),
+                success: color!(0x4BD130),
+                danger: color!(0xD5A30F),
+            },
+        )))
     }
 
-    container(stack)
-        .style(|_theme: &Theme| container::background(color!(0x000000)))
-        .padding([20, 30])
-        .into()
+    pub fn view(&self) -> Element<Message> {
+        let title = self
+            .user
+            .as_ref()
+            .and_then(|user_id| {
+                self.users
+                    .get(user_id)
+                    .map(|user_name| text(format!("{user_name} ({user_id})")))
+            })
+            .unwrap_or(text("Bitte RFID Chip"));
+
+        let sum = self.items.iter().map(|item| item.total()).sum::<Decimal>();
+
+        let content = column![
+            title.size(36),
+            scrollable(items(&self.items))
+                .height(Fill)
+                .width(Fill)
+                .anchor_bottom(),
+            text(format!("Summe: {sum:.2}€"))
+                .size(24)
+                .align_x(Right)
+                .width(Fill),
+            row![
+                button(
+                    text("Abbruch")
+                        .color(color!(0xffffff))
+                        .size(36)
+                        .align_x(Center)
+                )
+                .width(Fill)
+                .style(button::danger)
+                .padding([10, 20])
+                .on_press_maybe(self.user.as_ref().map(|_| Message::Cancel)),
+                button(
+                    text("Bezahlen")
+                        .color(color!(0xffffff))
+                        .size(36)
+                        .align_x(Center)
+                )
+                .width(Fill)
+                .style(button::success)
+                .padding([10, 20])
+                .on_press_maybe(self.user.as_ref().map(|_| Message::Pay)),
+            ]
+            .spacing(10),
+        ]
+        .spacing(10);
+
+        let mut stack = stack![content];
+
+        if self.show_sale_confirmation {
+            stack = stack.push(
+                container(
+                    container(
+                        text("Danke für deinen Kauf")
+                            .size(36)
+                            .color(color!(0x000000)),
+                    )
+                    .style(|_theme: &Theme| container::background(color!(0xffffff)))
+                    .padding([15, 30]),
+                )
+                .width(Fill)
+                .height(Fill)
+                .align_x(Center)
+                .align_y(Center),
+            );
+        }
+
+        container(stack)
+            .style(|_theme: &Theme| container::background(color!(0x000000)))
+            .padding([20, 30])
+            .into()
+    }
 }
 
 fn items(items: &[Item]) -> Element<Message> {
